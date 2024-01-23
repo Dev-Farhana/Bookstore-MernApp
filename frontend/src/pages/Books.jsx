ec255940c06e41e8db83d4e0cb49d2d6dfa11304
DevFarhana <124885288+Dev-Farhana@users.noreply.github.com>
2024-01-23 13:26:07 +0500

    Add files via upload

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import BookSection from '../components/BookSection';
+import Spinner from '../components/Spinner';
 
 function Books() {
   const [data, setData] = useState([]);
   const [error,setError] =  useState("");
+  const [loading, setLoading] = useState(true); // Initial loading state
+
 
   useEffect( () => {
     const fetchData = async() => {
       try{ 
-        const response =  await axios.get("http://localhost:2000/api/getbooks")
+        // const response =  await axios.get("http://localhost:4000/api/getbooks")
+        const response =  await axios.get("https://fine-jade-indri-kilt.cyclic.app/api/getbooks")
         setData(response.data.books);
         // console.log(response.data.books);
       }catch(err){
         setError(`Something went wrong data not Fetched!`);
+      }finally {
+        // Set loading to false regardless of success or failure
+        setLoading(false);
       }
     }
     fetchData();
@@ -25,9 +32,17 @@ function Books() {
         <h3 className='text-white'> Books Section </h3>
       </div>
       
-      { data ? ( <BookSection data={data} /> ) : 
-        (<h2 className='text-white py-3 fs-bold'> Loading... </h2>)
-      }
+      {loading ? (
+        // Render spinner or loading indicator
+        <Spinner />
+      ) : (
+        // Render your content when data is loaded
+        <BookSection data={data} />
+      )}
+
+      {/* { data ? ( <BookSection data={data} /> ) : 
+        (<h2 className='text-info py-3 fs-bold'> Loading... </h2>)
+      } */}
 
       { error && <div className="alert alert-danger w-25 text-center m-auto"> {error} </div> }
 
